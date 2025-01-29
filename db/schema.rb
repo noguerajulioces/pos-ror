d92ab894d3932ff575eca2570496f5dbfaceedb1
noguerajulioces <noguerajulio24@gmail.com>
2025-01-29 15:49:51 -0300

    add authentication user

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_29_181018) do
   enable_extension "pg_catalog.plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
