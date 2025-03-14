21b33a553ffc5ddfc7381a294b6c154034b40139
noguerajulioces <noguerajulio24@gmail.com>
2025-03-14 09:37:23 -0300

    add image

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,9 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import RailsNestedForm from "@stimulus-components/rails-nested-form"
+import PasswordVisibility from "@stimulus-components/password-visibility"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
 application.register("nested-form", RailsNestedForm)
+application.register("password-visibility", PasswordVisibility)
