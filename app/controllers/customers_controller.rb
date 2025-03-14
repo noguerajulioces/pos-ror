7ba4051740035b6662ac9b3586a3420aff7d86c9
noguerajulioces <noguerajulio24@gmail.com>
2025-03-14 08:43:56 -0300

    add search in product

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,8 @@ class CustomersController < ApplicationController
   before_action :set_customer, only: [ :show, :edit, :update, :destroy ]
 
   def index
-    @customers = Customer.all.paginate(page: params[:page], per_page: 10)
+    @q = Customer.ransack(params[:q])
+    @customers = @q.result(distinct: true).paginate(page: params[:page], per_page: 10)
   end
 
   def show
