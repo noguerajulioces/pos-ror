0c58f7bb7618466632b5100fb718b49b65c8efed
noguerajulioces <noguerajulio24@gmail.com>
2025-03-20 17:08:18 -0300

    feat: add clipboard functionality to product barcode display
    Enable users to copy the product barcode by clicking on it. This improves usability by allowing quick copying of the barcode for further use

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,8 +2,10 @@
 import { application } from "controllers/application"
 import RailsNestedForm from "@stimulus-components/rails-nested-form"
 import PasswordVisibility from "@stimulus-components/password-visibility"
+import Clipboard from '@stimulus-components/clipboard'
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
 application.register("nested-form", RailsNestedForm)
 application.register("password-visibility", PasswordVisibility)
+application.register('clipboard', Clipboard)
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@stimulus-components/rails-nested-form", to: "@stimulus-components--rails-nested-form.js" # @5.0.0
 pin "@stimulus-components/password-visibility", to: "@stimulus-components--password-visibility.js" # @3.0.0
+pin "@stimulus-components/clipboard", to: "@stimulus-components--clipboard.js" # @5.0.0
