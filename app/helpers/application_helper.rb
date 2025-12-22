8a98c6318b398d8d406270b027e3292ff4a3721c
Julio Noguera <noguerajulio24@gmail.com>
2025-12-22 14:30:23 -0300

    fix: Add helpers for stock comparisons to handle nil min_stock values

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
     return '' if product.min_stock.nil?
     product.stock <= product.min_stock ? 'bg-red-50' : ''
   end
+
+  def low_stock?(product)
+    return false if product.min_stock.nil?
+    product.stock <= product.min_stock
+  end
+
+  def stock_text_class(product, low_class, normal_class)
+    low_stock?(product) ? low_class : normal_class
+  end
 end
