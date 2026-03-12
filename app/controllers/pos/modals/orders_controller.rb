649f76ca314ab3852bed422808d991bae2afa25f
Julio Noguera <noguerajulio24@gmail.com>
2026-03-12 20:40:14 +0100

    feat: Display table name for on-hold orders in the modal and eager load associated table and customer data.

--- app/controllers/pos/modals/orders_controller.rb
+++ app/controllers/pos/modals/orders_controller.rb
@@ -2,7 +2,7 @@ module Pos
   module Modals
     class OrdersController < BaseController
       def index
-        @orders = Order.on_hold.order(created_at: :desc)
+        @orders = Order.on_hold.includes(:table, :customer).order(created_at: :desc)
         render 'pos/modals/orders_modal'
       end
     end
